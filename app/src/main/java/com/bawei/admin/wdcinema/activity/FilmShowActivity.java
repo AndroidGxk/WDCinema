81925562bc5647bd286eafa154a4e7e84027b674
Leeguanhui <45384649+Leeguanhui@users.noreply.github.com>
2019-01-24 15:27:31 +0800

    电影显示

---
+++ app/src/main/java/com/bawei/admin/wdcinema/activity/FilmShowActivity.java
@@ -0,0 +1,27 @@
+package com.bawei.admin.wdcinema.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bw.movie.R;
+
+import me.jessyan.autosize.internal.CustomAdapt;
+
+public class FilmShowActivity extends AppCompatActivity implements CustomAdapt {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_show);
+    }
+
+    @Override
+    public boolean isBaseOnWidth() {
+        return false;
+    }
+
+    @Override
+    public float getSizeInDp() {
+        return 720;
+    }
+}
