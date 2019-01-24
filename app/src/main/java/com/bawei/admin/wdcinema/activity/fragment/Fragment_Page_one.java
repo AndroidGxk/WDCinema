122916544ec49a2cff1dd8c3f03692ba41aace89
Leeguanhui <45384649+Leeguanhui@users.noreply.github.com>
2019-01-24 13:35:58 +0800

    定位

--- app/src/main/java/com/bawei/admin/wdcinema/activity/fragment/Fragment_Page_one.java
+++ app/src/main/java/com/bawei/admin/wdcinema/activity/fragment/Fragment_Page_one.java
@@ -10,7 +10,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.bawei.admin.wdcinema.adapter.Adapter;
 import com.bawei.admin.wdcinema.adapter.ComingSoonMovieAdapter;
 import com.bawei.admin.wdcinema.adapter.HotMovieAdapter;
@@ -47,10 +53,14 @@ public class Fragment_Page_one extends Fragment implements Adapter.onItemClick, 
     RecyclerView release_recycler;
     @BindView(R.id.comingSoon_recycler)
     RecyclerView comingSoon_recycler;
+    @BindView(R.id.location_tv)
+    TextView textView;
     private ReleaseMovieAdapter releaseMovieAdapter;
     private ComingSoonMovieAdapter comingSoonMovieAdapter;
     private ReleaseMoviePresenter releaseMoviePresenter;
     private ComingSoonMoviePresenter comingSoonMoviePresenter;
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
 
     @Nullable
     @Override
@@ -93,10 +103,42 @@ public class Fragment_Page_one extends Fragment implements Adapter.onItemClick, 
         comingSoonMovieAdapter = new ComingSoonMovieAdapter(getContext());
         comingSoon_recycler.setAdapter(comingSoonMovieAdapter);
 
+        //百度定位
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
+        //可选，是否需要位置描述信息，默认为不需要，即参数为false
+        //如果开发者需要获得当前点的位置信息，此处必须为true
+        option.setIsNeedLocationDescribe(true);
+        //可选，设置是否需要地址信息，默认不需要
+        option.setIsNeedAddress(true);
+        //可选，默认false,设置是否使用gps
+        option.setOpenGps(true);
+        //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setLocationNotify(true);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+
         hotMoviePresenter.request(userId, sessionId, 1, 10);
         releaseMoviePresenter.request(userId, sessionId, 1, 10);
         comingSoonMoviePresenter.request(userId, sessionId, 1, 10);
         return view;
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取地址相关的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
+            String addr = location.getAddrStr();    //获取详细地址信息
+            textView.setText(locationDescribe + addr);
+
+        }
     }
 
     @Override
